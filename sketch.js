e031a14683327f73178350b2f812c1e47e4dce73
AlwaysReal:git config --global user.name AlwaysReal <cwchanin26@wabash.edu>
2026-04-02 00:09:15 -0400

    Trails added after hovering over grid, button made for changing size

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const userInput = document.querySelector(".userInput");
 
 function makeDivs(gridSize){
     const cellSize = 100 / gridSize
@@ -11,3 +12,28 @@ function makeDivs(gridSize){
 }
 
 makeDivs(16);
+
+container.addEventListener('mouseover', function(e){
+    if(e.target.matches('#container div')){
+        e.target.classList.toggle('coloredBox');
+    }
+});
+
+userInput.textContent = "Click me to change grid size";
+userInput.addEventListener('click', function(e){
+    let newGrid = prompt("Enter grid size:");
+    
+    if (newGrid === null) return;
+
+    newGrid = Number(newGrid);
+
+    if (newGrid > 100 || newGrid <= 0){
+        alert("Please enter a number between 1 and 100");
+        handleClick();
+        return;
+    }
+    container.innerHTML = "";
+    makeDivs(newGrid);
+    }
+);
+
